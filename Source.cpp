08166d649b5ee0318e38e484581eab39b12deae2
Baudin <baudi383860@gmail.com>
2022-10-25 22:21:15 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,42 +3,6 @@
 
 using namespace sf;
 
-
-
-
-//const int w = 10, h = 15;//1
-//bool x = false, gameover = false;
-//int arr[h][w] = { 0 };
-
-//struct point
-//{
-//	int x, y;
-//}a[4], b[4];
-//
-//int arr2[7][4] =
-//{
-//	1,3,5,7,
-//	2,4,5,7,
-//	3,5,4,6,
-//	3,5,4,7,
-//	2,3,5,7,
-//	3,5,7,6,
-//	2,3,4,5
-//};
-//
-//bool check()
-//{
-//	for (int i = 0; i < 4; i++)
-//	{
-//		if (a[i].x < 0 | a[i].x > w-1 | a[i].y >= h)
-//			return true;
-//
-//		if (arr[a[i].y][a[i].x])
-//			return true;
-//	}
-//
-//	return false;
-//};
 const int size = 10;
 int bomb_count = 5;
 bool gameOver = false;
@@ -98,25 +62,13 @@ int main()
 			i++;
 		}
 	}
-
 	for each(Field var in fields)
 	{
 		if(var.status!=bomb)
 		var.value=v(var, size);
 	}
 
-	//setlocale(LC_ALL, "ru");
-	//srand(time(0));
 	RenderWindow window(sf::VideoMode(200, 200), "-----");
-
-	//Image i;
-	//i.loadFromFile("C://Users/User/Desktop/плитка.png");
-	//i.createMaskFromColor(Color(255, 255, 255));
-	//Texture t,t2;
-	//t.loadFromFile("C://Users/User/Desktop/фон.png");
-	//t2.loadFromImage(i);
-	//Texture t,t2;
-	//t.loadFromFile("C://Users/User/Desktop/фон.png");
 
 		Texture t_VisibleTrue, t_VisibleFalse;
 		t_VisibleTrue.loadFromFile("C://Users/User/Desktop/VisibleTrue.png");
@@ -125,61 +77,15 @@ int main()
 		Sprite s_VisibleTrue,s_VisibleFalse;
 		s_VisibleTrue.setTexture(t_VisibleTrue);
 		s_VisibleFalse.setTexture(t_VisibleFalse);
-		//s.setPosition(0, 0);
-	/*Clock clock;
-	float timer=0;
-	float d = 0.7;
-	bool Start = true;
-	bool rotate = false;
-	int n = rand() % 7;
-	if (n == 6)
-		x = true;
-	
-	int dx = 0;
-	
-
-	
-
-	
-
-	Sprite s,s1;
-	s.setTexture(t);
-	s.setPosition(0, 0);
-	s1.setTexture(t2);
-	s1.setPosition(90, 0);
-*/
-	while (window.isOpen()/*&& gameover ==false*/)
+		
+	while (window.isOpen())
 	{
-
-		/*
-		if (Start)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				a[i].x = arr2[n][i] % 2 +4;
-				a[i].y = arr2[n][i] / 2 - 1;
-			}
-			Start = false;
-		}
-
-		float time = clock.getElapsedTime().asSeconds();
-		clock.restart();
-		timer += time;*/
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-			/*if (Keyboard::isKeyPressed(Keyboard::D))
-			{
-				dx += 1;
-			}
-			if (Keyboard::isKeyPressed(Keyboard::A))
-				dx -= 1;
-			if (Keyboard::isKeyPressed(Keyboard::S))
-				d = 0.2;
-			if (Keyboard::isKeyPressed(Keyboard::W))
-				rotate = true;*/
+			
 			if (!gameOver)
 			{
 				if (Mouse::isButtonPressed(Mouse::Left))
@@ -204,99 +110,6 @@ int main()
 			}
 		}
 
-		/*for (int i = 0; i < 4; i++)
-		{
-			b[i] = a[i];
-			a[i].x += dx/2;
-		}
-		if (check())
-			for (int i = 0; i < 4; i++)
-			{
-				a[i] = b[i];
-			}
-		
-		if (rotate)
-		{
-			if (x){}
-			
-			else
-			{
-				point p = a[1];
-				for (int i = 0; i < 4; i++)
-				{
-					b[i] = a[i];
-				}
-				for (int i = 0; i < 4; i++)
-				{
-
-					int x = a[i].y - p.y;
-					int y = a[i].x - p.x;
-					a[i].x = p.x - x;
-					a[i].y = p.y + y;
-				}
-				if (check())
-					for (int i = 0; i < 4; i++)
-					{
-						a[i] = b[i];
-					}
-
-				rotate = false;
-			}
-		}
-		
-		
-		if (timer > d)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				b[i] = a[i];
-				a[i].y += 1;
-			}
-			if (check())
-			{
-				int n = rand() % 7;
-				if (n == 6)
-					x = true;
-				else
-					x = false;
-				for (int i = 0; i < 4; i++)
-				{
-					arr[b[i].y][b[i].x] = 1;
-				}
-				for (int i = 0; i < 4; i++)
-				{
-					a[i].x = arr2[n][i] % 2 + 4;
-					a[i].y = arr2[n][i] / 2 - 1;
-				}
-				for (int i = 0; i < 4; i++)
-				{
-					if (arr[a[i].y][a[i].x])
-					{
-						gameover = true;
-					}
-				}
-			}
-			timer = 0;
-		}
-		int k = h - 1;
-		for (int i = h-1; i > 0; i--)
-		{
-			int count = 0;
-			for (int j = 0; j < w;j++)
-			{
-				if (arr[i][j])
-				{
-					count++;
-				}
-				arr[k][j] = arr[i][j];
-			}
-			if (count<w)
-				k--;
-			else
-			{
-				k = k;
-			}
-		}*/
 		window.clear();
 		for (int i = 0; i < size; i++)
 		{
@@ -336,35 +149,8 @@ int main()
 				}
 			}
 		}
-
-
-
-
-
-
-
-		//window.draw();
-		/*if (!gameover)
-			for (int i = 0; i < 4; i++)
-			{
-				s1.setPosition(a[i].x * 20, a[i].y * 20);
-				window.draw(s1);
-			}
-		
-		for (int i = 0; i < h; i++)
-		{
-			for (int j = 0; j < w; j++)
-			{
-				if (arr[i][j])
-				{
-					s1.setPosition(j * 20,i * 20);
-					window.draw(s1);
-				}
-			}
-		}*/
+	
 		window.display();
-		/*dx = 0;
-		d = 0.7;*/
 	}
 	return 0;
 }
